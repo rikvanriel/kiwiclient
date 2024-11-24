9796049d65a3c9af7ac57c74848440b2ea8b8245
Rik van Riel <riel@surriel.com>
2024-11-24 13:36:17 -0500

    rigctld: also parse F VFOA 1234.00000 format
    With Fedora 41, fldigi and hamlib now seem to send the frequency
    setting command (F) in a slightly different format:
    
    F VFOA 1234.00000
    
    Add something to parse that, and also an error message when we do
    not succeed in parsing the set frequency command.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>

--- kiwi/rigctld.py
+++ kiwi/rigctld.py
@@ -108,8 +108,19 @@ class Rigctld(object):
     def _set_frequency(self, command):
         try:
             # hamlib freq is in Hz, kiwisdr in kHz
+            # format: F 1234.00000
             newfreq = command[2:]
             freq = float(newfreq) / 1000
+        except:
+            try:
+                # format: F VFOA 1234.00000
+                newfreq = command[7:]
+                freq = float(newfreq) / 1000
+            except:
+                print(f"could not decode frequency from {command}")
+                return "RPRT -1\n"
+
+        try:
             mod = self._kiwisdrstream.get_mod()
             lc = self._kiwisdrstream.get_lowcut()
             hc = self._kiwisdrstream.get_highcut()
